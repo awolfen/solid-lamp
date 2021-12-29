16a4b4b7bf932ff5c755c9d2d9534d04178b3942
awolfen <alfie.wolfenden@gmail.com>
2021-12-29 17:08:28 +0000

    static paths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ const MeetupDetails = () => {
     )
 };
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                }
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    //fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Cabot_Tower_%28600px%29.jpg',
+                title: 'First Meetup',
+                address: 'Some adress',
+                description: 'This is a first meetup'
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
